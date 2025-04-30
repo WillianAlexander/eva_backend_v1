fb534553ff9863e17a07912a1c07ba77ec5b5b06
CacpgUI <wlucero@coopgualaquiza.fin.ec>
2025-04-30 16:56:53 -0500

    Cambio para registro y control de evaluaciones

--- backend-eva/src/controllers/evaluaciones.controller.ts
+++ backend-eva/src/controllers/evaluaciones.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  Body,
+  Controller,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { RegistrarEvaluacionDto } from 'src/dto/evaluacion/registrar-evaluacion.dto';
 import { Evaluaciones } from 'src/entities/evaluaciones.entity';
 import { EvaluacionesService } from 'src/services/evaluaciones.service';
@@ -8,12 +16,33 @@ export class EvaluacionesController {
   constructor(private evaluacionService: EvaluacionesService) {}
 
   @Post('find')
-  findByAny(@Body() options: Partial<Evaluaciones>) {
+  async findByAny(@Body() options: Partial<Evaluaciones>) {
     return this.evaluacionService.findByAny(options);
   }
 
   @Post()
-  registerEvaluation(@Body() dto: RegistrarEvaluacionDto) {
-    return this.evaluacionService.registerEvaluation(dto);
+  async registerEvaluation(@Body() dto: RegistrarEvaluacionDto) {
+    try {
+      return await this.evaluacionService.registerEvaluation(dto);
+    } catch (error) {
+      if (error.code === '23505') {
+        // Error de llave duplicada
+        throw new HttpException(
+          {
+            status: HttpStatus.CONFLICT,
+            message: error.detail, // Enviar el mensaje exacto del error
+          },
+          HttpStatus.CONFLICT,
+        );
+      }
+      // Otros errores
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error interno del servidor',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
